263286ab7344e6981732a4f24387392f96f60511
amarka8 <[amar.kanakamedala@gmail.com]>
2025-06-06 17:01:34 -0500

    build train-val-test almost done

--- clustering.py
+++ clustering.py
@@ -1,25 +1,52 @@
 import os
 import math
+import numpy as np
+import PyPDF2
 
-def main():
+def build_train_val_test():
     # use os module to get list of pdfs to train and test on
     dir_path = "/Users/amarkanaka/Desktop/server_baker_files_final"
     file_lst = os.listdir(dir_path)
     file_lst = [dir_path + "/" + file_name for file_name in file_lst]
     train_len = math.floor(len(file_lst) * 0.125)
-    val_len = train_len
-    test_len = len(file_lst) - val_len - train_len
-    # create folders to be itemized 
+    test_len = len(file_lst) - 2 * train_len 
+    #build train test and val sets through random sampling
+    used_set = set()
+    train = []
+    val = []
+    test = []
+
+    while len(train) < train_len:
+        idxs = np.random.choice(len(file_lst), train_len - len(train))
+        for idx in idxs:
+            if idx not in used_set:
+                train.append(file_lst[idx])
+                used_set.add(idx)
+
+    
+    # DEBUG
+    print(train)
+    print("\n")
 
 
-    # names = []
-    # for name in glob(f'{path_list}/*'):
-    #     name = name.split('/')[-1]
-    #     names.append(name)
+    for file_name in train:
 
-    # for i, n in enumerate(np.random.poisson(lam, samples)):
-    #     for name in np.random.choice(names, n):
-    #         print(name, i + 1)
+        # DEBUG
+        print(file_name)
+        print("\n")
+
+        write_file = open("test.txt", "a")
+
+        pdfReader = PyPDF2.PdfReader(open(file_name, "r"))
+        # count number of pages
+        totalPages = len(pdfReader.pages)
+
+        for page in range(totalPages):
+            write_file.write(str(page)+"        "+file_name+"\n")
+
+def main():
+    build_train_val_test()
+
 
 
 if __name__ == "__main__":
