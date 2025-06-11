4c95ddcb1d4092d098f47a6250b9f86d22dbd1fd
amarka8 <[amar.kanakamedala@gmail.com]>
2025-06-11 11:27:27 -0500

    added files

--- sampleDocs.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def sample(names, samples, lam = 11):
-    
-
-    for i, n in enumerate(np.random.poisson(lam, samples)):
-        for name in np.random.choice(names, n):
-            print(name, i + 1)
