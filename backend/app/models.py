17d8b42bfe631a0054810cea3ce73d19625c1804
amarka8 <[amar.kanakamedala@gmail.com]>
2025-06-11 20:43:08 -0500

    debug issues 2

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,26 +1,36 @@
 from typing import Annotated, List
 
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
 from contextlib import asynccontextmanager
 from sqlalchemy import JSON, Column
 
 
-
 # id, name, collection, title, people+orgs, location, description, date, subjects, accessibility
-class Hero(SQLModel, table=True):
+class Doc(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     collection: str = Field(index=True)
     title: str = Field(index=True)
-    people_plus_orgs: List[str] = Field (index = True)
+    entities: list["PeopleOrgs"] = Relationship(back_populates="people_plus_orgs")
     # index not enabled on following 2
-    location: List[str] | None = Field(default = None)
+    # location: str | None = Field(default = None)
     description: str = Field()
     date: int | None = Field(default = None, index = True)
-    subjects: List[str] = Field()
+    subject_lst: list["Subjects"] = Relationship(back_populates="subjects")
     # index not enabled
     accessibility: str | None = Field(default=None)
+
+class PeopleOrgs(SQLModel, table = True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index = True)
+    people_plus_orgs: Doc | None = Relationship(back_populates="entities")
+
+class Subjects(SQLModel, table = True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index = True)
+    subjects: Doc | None = Relationship(back_populates="subject_lst")
+
 
 
 sqlite_file_name = "database.db"
@@ -61,7 +71,7 @@ CRUD
 
 # submit data (pdf to ocr, itemize, and extract metadata)
 @app.post("/heroes")
-def create_db_obj(hero: Hero, session: SessionDep) -> Hero:
+def create_db_obj(hero: Doc, session: SessionDep) -> Doc:
     session.add(hero)
     session.commit()
     session.refresh(hero)
@@ -70,24 +80,24 @@ def create_db_obj(hero: Hero, session: SessionDep) -> Hero:
 
 # retrieve data (in our case, we are retrieving metadata + PDF to serve to users)
 @app.get("/heroes")
-def retrieve_db_objs(session: SessionDep, offset: int, limit: Annotated[int, Query(le = 100)]) -> List[Hero]:
-    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+def retrieve_db_objs(session: SessionDep, offset: int, limit: Annotated[int, Query(le = 100)]) -> list[Doc]:
+    heroes = session.exec(select(Doc).offset(offset).limit(limit)).all()
     return heroes
 
 # retrieve one piece of data
 
 
 @app.get("/heroes/{hero_id}")
-def retrieve_db_objs(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.exec(select(Hero).where(Hero.id == hero_id))
+def retrieve_db_objs(hero_id: int, session: SessionDep) -> Doc:
+    hero = session.exec(select(Doc).where(Doc.id == hero_id))
     if not hero:
         raise HTTPException(404, "id not found")
     return hero
 
 
 @app.delete("/heroes/{hero_id}")
-def remove_db_objs(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.exec(select(Hero).where(Hero.id == hero_id))
+def remove_db_objs(hero_id: int, session: SessionDep) -> Doc:
+    hero = session.exec(select(Doc).where(Doc.id == hero_id))
     if not hero:
         raise HTTPException(404, "id not found")
     session.delete(hero)
