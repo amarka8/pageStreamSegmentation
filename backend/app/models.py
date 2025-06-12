4c1d00011f48d32d813875d3ef117870d6c9b3b0
amarka8 <[amar.kanakamedala@gmail.com]>
2025-06-11 20:17:24 -0500

    debug issues

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,21 +1,24 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from contextlib import asynccontextmanager
+from sqlalchemy import JSON, Column
 
 
+
+# id, name, collection, title, people+orgs, location, description, date, subjects, accessibility
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     collection: str = Field(index=True)
     title: str = Field(index=True)
-    people_plus_orgs: list[str] = Field (index = True)
+    people_plus_orgs: List[str] = Field (index = True)
     # index not enabled on following 2
-    location: list[str] | None = Field(default = None)
+    location: List[str] | None = Field(default = None)
     description: str = Field()
     date: int | None = Field(default = None, index = True)
-    subjects: list[str] = Field(index = True)
+    subjects: List[str] = Field()
     # index not enabled
     accessibility: str | None = Field(default=None)
 
@@ -52,6 +55,9 @@ async def lifespan(app: FastAPI):
 # for development - change to use migrations eventually
 app = FastAPI(lifespan=lifespan)
 
+"""
+CRUD
+"""
 
 # submit data (pdf to ocr, itemize, and extract metadata)
 @app.post("/heroes")
@@ -62,8 +68,32 @@ def create_db_obj(hero: Hero, session: SessionDep) -> Hero:
     # why ???????
     return hero
 
-
-# retrieve data (in our casee, we are retrieving metadata + PDF to serve to users)
+# retrieve data (in our case, we are retrieving metadata + PDF to serve to users)
 @app.get("/heroes")
-def get_db_obj(hero_id: int, session: SessionDep, offset: int, limit: Annotated[int, Query(le = 100)]) -> list[Hero]:
-    heroes = session.exec(select(Hero).where(Hero.id == hero_id).offset(offset).limit(limit)).all()
+def retrieve_db_objs(session: SessionDep, offset: int, limit: Annotated[int, Query(le = 100)]) -> List[Hero]:
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+# retrieve one piece of data
+
+
+@app.get("/heroes/{hero_id}")
+def retrieve_db_objs(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.exec(select(Hero).where(Hero.id == hero_id))
+    if not hero:
+        raise HTTPException(404, "id not found")
+    return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def remove_db_objs(hero_id: int, session: SessionDep) -> Hero:
+    hero = session.exec(select(Hero).where(Hero.id == hero_id))
+    if not hero:
+        raise HTTPException(404, "id not found")
+    session.delete(hero)
+    # FFLUSH / Msync type operation
+    session.commit()
+    return {"ok": True}
+
+
+
